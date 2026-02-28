2cc7f8e66ce0de1a793a98664199959cbdbb7c11
ibrahim ezzine <ibrahimezzine09@gmail.com>
2026-02-28 15:28:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-788884078-2c6f0",
+  "appId": "1:939651207197:web:a6b69ea1ffd13fa52276f4",
+  "apiKey": "AIzaSyDnmMORZNI1YO-IzJEXmmlV-4Z3TiBhhrE",
+  "authDomain": "studio-788884078-2c6f0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "939651207197"
+};
